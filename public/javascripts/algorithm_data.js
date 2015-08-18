ddda4c545eba115e1c89d1fc337e0cf8df4ae251
Tom Fewster <tom@wannabegeek.com>
2015-08-18 08:57:29 +0100

    another update

--- public/javascripts/algorithm_data.js
+++ public/javascripts/algorithm_data.js
@@ -1,6 +1,6 @@
 $(function() {
-    display_data(null);
-}
+//     display_data(null);
+});
 
 function display_data(data) {
     // chartData = [];
@@ -14,27 +14,112 @@ function display_data(data) {
     //     maxVal = Math.max(maxVal, value);
     //     chartData[chartData.length] = [parseFloat(d["timestamp"] * 1000), value];
     // }
+    // 
+    // $('#performance_chart').highcharts('StockChart', {
+    //     rangeSelector: {
+    //         selected: 6,
+    //         inputEnabled: false,
+    //     },
 
-    $('#performance_chart').highcharts('StockChart', {
-        rangeSelector: {
-            selected: 6,
-            inputEnabled: false,
-        },
+    //     title : {
+    //         text : 'Stock Price'
+    //     },
 
-        title : {
-            text : 'Stock Price'
-        },
+    //     series : [{
+    //         type : 'area',
+    //         name : 'SID #' + sid,
+    //         // data : chartData,
+    //         tooltip: {
+    //             valueDecimals: 2
+    //         },
+    //         lineWidth: 1,
+    //         threshold: null,
+    //         fillColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.1).get('rgba'),
+    //     }],
+    // });
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function (data) {
+        // split the data set into ohlc and volume
+        var ohlc = [],
+            volume = [],
+            dataLength = data.length,
+            // set the allowed units for data grouping
+            groupingUnits = [[
+                'week',                         // unit name
+                [1]                             // allowed multiples
+            ], [
+                'month',
+                [1, 2, 3, 4, 6]
+            ]],
 
-        series : [{
-            type : 'area',
-            name : 'SID #' + sid,
-            // data : chartData,
-            tooltip: {
-                valueDecimals: 2
+            i = 0;
+
+        for (i; i < dataLength; i += 1) {
+            ohlc.push([
+                data[i][0], // the date
+                data[i][1], // open
+                data[i][2], // high
+                data[i][3], // low
+                data[i][4] // close
+            ]);
+
+            volume.push([
+                data[i][0], // the date
+                data[i][5] // the volume
+            ]);
+        }
+
+
+        // create the chart
+        $('#performance_chart').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 1
             },
-            lineWidth: 1,
-            threshold: null,
-            fillColor: Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0.1).get('rgba'),
-        }],
+
+            title: {
+                text: 'AAPL Historical'
+            },
+
+            yAxis: [{
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'OHLC'
+                },
+                height: '60%',
+                lineWidth: 2
+            }, {
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'Volume'
+                },
+                top: '65%',
+                height: '35%',
+                offset: 0,
+                lineWidth: 2
+            }],
+
+            series: [{
+                type: 'candlestick',
+                name: 'AAPL',
+                data: ohlc,
+                dataGrouping: {
+                    units: groupingUnits
+                }
+            }, {
+                type: 'column',
+                name: 'Volume',
+                data: volume,
+                yAxis: 1,
+                dataGrouping: {
+                    units: groupingUnits
+                }
+            }]
+        });
     });
 }
