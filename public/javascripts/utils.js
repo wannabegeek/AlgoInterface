c4a646ad93fd416132b604009779a4efc7555e32
Tom Fewster <tom@wannabegeek.com>
2015-08-19 23:36:13 +0100

    more updates

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,6 +1,10 @@
 $(function() {
-    var resize= $("#split_pane #top_pane");
-    var containerHeight = $("#split_pane").height();
+    var split_pane = $("#split_pane");
+    var resize = $("#split_pane #top_pane");
+
+    // this accounts for padding in the panels + 
+    // borders, you could calculate this using jQuery
+    var padding = 12; 
         
     $(resize).resizable({
           handles: 's',
@@ -9,17 +13,24 @@ $(function() {
           resize: function(event, ui){
               var currentHeight = ui.size.height;
               
-              // this accounts for padding in the panels + 
-              // borders, you could calculate this using jQuery
-              var padding = 12; 
-              
               // this accounts for some lag in the ui.size value, if you take this away 
               // you'll get some instable behaviour
               $(this).height(currentHeight);
               
               // set the content panel width
-              $("#split_pane #bottom_pane").height(containerHeight - currentHeight - padding); 
+              $("#split_pane #bottom_pane").height($(split_pane).height() - currentHeight - padding); 
               hs_chart.reflow()
           }
     });
+
+    function setSplitPaneSize() {
+      var new_height = $(window).height() - $("#split_pane").offset().top - 12;
+      $("#split_pane").height(new_height);
+      $(resize).height(new_height - padding - $("#split_pane #bottom_pane").height);
+      hs_chart.reflow();
+    };
+
+    $(window).resize(setSplitPaneSize);
+
+    setSplitPaneSize();
 });
